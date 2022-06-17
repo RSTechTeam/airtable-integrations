96b9f27e30950c0b9fae92e2d1f6977f345ade48
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-06-17 14:09:03 -0700

    Create accounting_sync.js

---
+++ src/accounting_sync.js
@@ -0,0 +1,61 @@
+/** @fileoverview Syncs Bill.com Customers from Airtable to Bill.com. */
+
+import * as airtable from './airtable.js';
+import * as billCom from './bill_com.js';
+import * as utils from './utils.js';
+
+/** The Airtable Table name Labor Charge Fields. */
+const LCF_TABLE = 'Labor Charge Field (LCF) Mapping';
+
+/** The Parent Bill.com Customer ID for internal Customers. */
+const internalCustomerId = utils.getInput('internal-customer-id');
+
+// Initialize Bill.com Customer collection.
+await billCom.primaryOrgLogin();
+const billComCustomers =
+    await billCom.list(
+        'Customer',
+        [billCom.filter('parentCustomerId', '=', internalCustomerId)]);
+const billComCustomerIds = new Set();
+billComCustomers.forEach(c => billComCustomerIds.add(c.id));
+
+// Upsert every Bill.com Customer from the Bill.com Sync View.
+const updates = [];
+airtable.select(
+    LCF_TABLE,
+    'Bill.com Sync',
+    (r) => {
+      const id = r.get(airtable.primaryOrgBillComId);
+      const change = {
+        obj: {
+          entity: 'Customer',
+          isActive: '1',
+          parentCustomerId: internalCustomerId,
+          name: encodeURIComponent(r.get('Abacus / Bill.com / QBO Code')),
+        }
+      }
+
+      // Insert/Create in Bill.com any record with no primary org Bill.com ID.
+      if (id.length === 0) {
+        const response =
+            await billCom.commonDataCall('Crud/Create/Customer', change);
+        airtable.update(
+            LCF_TABLE,
+            [{
+              id: r.getId(),
+              fields: {[airtable.primaryOrgBillComId]: response.id},
+            }]);
+        continue;
+      }
+
+      // Update in Bill.com other records with a primary org Bill.com ID.
+      change.obj.id = id;
+      updates.push(change);
+      billComCustomerIds.delete(id);
+    });
+
+// Mark internal Bill.com Customers not in the Bill.com Sync View as inactive.
+for (const id of billComCustomerIds) {
+  updates.push({obj: {entity: 'Customer', id: id, isActive: '2'}});
+}
+await billCom.bulkCall('Update/Customer', updates);
