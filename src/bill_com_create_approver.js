03bc0678e0887ae2f0cd8f846e856b54f9f9105d
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-06-17 14:12:30 -0700

    Create bill_com_create_approver.js

---
+++ src/bill_com_create_approver.js
@@ -0,0 +1,31 @@
+/** @fileoverview Creates a Bill.com Electronic Check Request Approver. */
+
+import * as airtable from './airtable.js';
+import * as billCom from './bill_com.js';
+import * as utils from './utils.js';
+
+/** The Airtable Table name for new Bill.com approvers. */
+const APPROVER_TABLE = 'New Bill.com Approvers';
+
+/** The Bill.com User Profile ID for ECR Approvers. */
+const ecrApproverUserProfileId = utils.getInput('ecr-approver-user-profile-id');
+
+await billCom.primaryOrgLogin();
+airtable.select(
+    APPROVER_TABLE,
+    'New',
+    (record) => {
+      await billCom.commonDataCall(
+          'Crud/Create/User',
+          {
+            obj: {
+              entity: 'User',
+              profileId: ecrApproverUserProfileId,
+              firstName: record.get('First Name'),
+              lastName: record.get('Last Name'),
+              email: record.get('Email'),
+            }
+          });
+      airtable.update(
+          APPROVER_TABLE, [{id: record.getId(), fields: {'Created': true}}]);
+    });
