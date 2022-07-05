7c1f7d1bab133cddfe052276235f72700db8ef6c
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-07-05 16:01:11 -0700

    Split info calls with stringify

--- dist/index.js
+++ dist/index.js
@@ -18545,7 +18545,8 @@ function fetchError(code, context, message) {
  */
 function logJson(name, json) {
   // core.startGroup(name);
-  _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(name, JSON.stringify(json));
+  _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(name);
+  _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(JSON.stringify(json));
   // core.endgroup();
 }
 
