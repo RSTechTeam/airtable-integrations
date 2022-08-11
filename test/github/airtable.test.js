e3f375aa643a826e44e0c833914406692dfa45e1
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-08-11 15:06:11 -0700

    Finish airtable select tests

--- test/github/airtable.test.js
+++ test/github/airtable.test.js
@@ -6,9 +6,20 @@ const base =
 const table = 'Table 1';
 
 describe('select', () => {
-  test('with no view defaults', async () => {
+  const selectIds = async (view) => {
     const ids = [];
-    await base.select(table, undefined, (r) => ids.push(r.get('ID')));
-    expect(ids).toEqual(expect.arrayContaining([1, 2, 3]));
+    await base.select(table, view, (r) => ids.push(r.get('ID')));
+    return ids;
+  }
+
+  test('with no view defaults to whole table', () => {
+    return expect(selectIds(undefined)).resolves.toEqual(
+        expect.arrayContaining([1, 2, 3]));
   });
+
+  test('uses given view', async () => {
+    const ids = await selectIds('No One');
+    expect(ids).toEqual(expect.arrayContaining([2, 3]));
+    expect(ids).toEqual(expect.not.arrayContaining([1]));
+  })
 });
