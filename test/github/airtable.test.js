d41762c36523a1df4a91e3566a78cb338a4cfd34
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-08-11 21:09:08 -0700

    Add airtable update test

--- test/github/airtable.test.js
+++ test/github/airtable.test.js
@@ -4,22 +4,51 @@ const base =
     new airtable.Base(
         process.env.AIRTABLE_BASE_ID, process.env.AIRTABLE_API_KEY);
 const table = 'Table 1';
+const select = (view, func) => base.select(table, view, func);
+const recordIds = new Map();
+const selectField = async (view, field) => {
+  const vals = [];
+  await select(view, (r) => vals.push(r.get(field)));
+  return vals;
+}
 
 describe('select', () => {
-  const selectIds = async (view) => {
-    const ids = [];
-    await base.select(table, view, (r) => ids.push(r.get('ID')));
-    return ids;
-  }
 
-  test('with no view defaults to whole table', () => {
-    return expect(selectIds(undefined)).resolves.toEqual(
-        expect.arrayContaining([1, 2, 3]));
+  test('with no view defaults to whole table', async () => {
+    const ids = [];
+    await select(
+      undefined,
+      (r) => {
+        const id = r.get('ID');
+        ids.push(id);
+        recordIds.set(id, r.getId()); // Init recordIds
+      });
+    expect(ids).toEqual(expect.arrayContaining([1, 2, 3]));
   });
 
   test('uses given view', async () => {
-    const ids = await selectIds('No One');
+    const ids = await selectField('No One', 'ID');
     expect(ids).toEqual(expect.arrayContaining([2, 3]));
     expect(ids).toEqual(expect.not.arrayContaining([1]));
-  })
+  });
+});
+
+test('update', async () => {
+  const expectTextsToContain =
+      (expected) => expect(selectField(undefined, 'Text')).resolves.toEqual(
+          expect.arrayContaining(expected));
+  await expectTextsToContain(['Hello', 'World', '!']);
+
+  const update = (text1, text3) => {
+    return base.update(table, [
+      {id: recordIds.get(1), fields: {text: text1}},
+      {id: recordIds.get(3), fields: {text: text3}},
+    ]);
+  };
+  const result = await update('Goodbye', '?');
+  console.log(result);
+  await expectTextsToContain(['Goodbye', 'World', '?']);
+
+  await update('Hello', '!');
+  await expectTextsToContain(['Hello', 'World', '!']);
 });
