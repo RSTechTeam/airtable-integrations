ebf1757bd1daa9a5abdd5b8fa66cdd32cfc8f9db
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-08-15 12:17:02 -0700

    Add some create tests

--- test/github/airtable.test.js
+++ test/github/airtable.test.js
@@ -7,6 +7,7 @@ const table = 'Table 1';
 const select = (view, func) => base.select(table, view, func);
 const recordIds = new Map();
 const selectField = (view, field) => select(view, (r) => r.get(field));
+const selectId = (view) => selectField(view, 'ID');
 
 describe('select', () => {
 
@@ -27,11 +28,11 @@ describe('select', () => {
   });
 
   test('given unknown view, throws', () => {
-    return expect(selectField('Unknown', 'ID')).rejects.toThrow();
+    return expect(selectId('Unknown')).rejects.toThrow();
   });
 
   test('uses given view', async () => {
-    const ids = await selectField('No One', 'ID');
+    const ids = await selectField('No One');
     expect(ids).toEqual(expect.arrayContaining([2, 3]));
     expect(ids).toEqual(expect.not.arrayContaining([1]));
   });
@@ -66,5 +67,28 @@ describe('update', () => {
 
     await update(table, 'Hello', '!');
     await expectTextsToContain(['Hello', 'World', '!']);
+  });
+});
+
+describe('create', () => {
+
+  test('given empty, return empty', () => {
+    return expect(base.create(table, []).resolves.toEqual([]));
+  });
+
+  const create = (tbl) => {
+    base.create(tbl, [{fields: {ID: 4}}, {fields: {ID: 5}}]);
+  };
+
+  test('given no table (with non-empty creates), throws', () => {
+    expect(() => create('')).toThrow();
+  });
+
+  test('creates records', async () => {
+    const created = await create(table);
+    console.log(created);
+    const ids = await selectId('');
+    expect(ids.length).toBe(5);
+    expect(ids).toEqual(expect.arrayContaining([1, 2, 3, 4, 5]));
   });
 });
