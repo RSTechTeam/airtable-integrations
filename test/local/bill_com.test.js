fcfc12cb0d07b4e0fa5e8eb7394f08ba83a41ba3
RSMvmtTech <94406834+RSMvmtTech@users.noreply.github.com>
2022-08-20 14:47:42 -0700

    Add more local bill_com tests

--- test/local/bill_com.test.js
+++ test/local/bill_com.test.js
@@ -2,13 +2,27 @@ import * as billCom from '../../src/bill_com.js';
 
 describe('apiCall', () => {
 
-  const expectApiCallToThrow = (endpoint, errorCode) => () => {
-    const apiCall = billCom.apiCall(endpoint, {}, '', true);
+  const expectApiCallToThrow = (endpoint, appType, body, errorCode) => () => {
+    const headers = {'Content-Type': `application/${appType}`};
+    const apiCall = billCom.apiCall(endpoint, headers, body, true);
     return expect(apiCall).rejects.toThrow(new RegExp(`BDC_${errorCode}`));
   };
 
-  test('given no endpoint, throws', expectApiCallToThrow('', 1121));
-  test('given missing params, throws', expectApiCallToThrow('Login', 1108));
+  const expectLoginToThrow = (appType, body, errorCode) => {
+    return expectApiCallToThrow('Login', appType, body, errorCode);
+  };
+
+  const body = 'devKey=devKey&userName=userName&password=password&orgId=orgId';
+
+  test('given no endpoint, throws', expectApiCallToThrow('', '', '', 1121));
+
+  test('given missing params, throws', expectLoginToThrow('', '', 1108));
+
+  test('given invalid params (wrong Content-Type), throws',
+      expectLoginToThrow('', body, 1108));
+
+  test('given invalid params (correct Content-Type, throws',
+      expectLoginToThrow('x-www-form-urlencoded', body, 1129));
 });
 
 test('filter', () => {
