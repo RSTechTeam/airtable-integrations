81457a6130f55a3fca2f090eace209492cb36b88
JustisAllen <justisallen@me.com>
2024-03-05 23:07:22 -0800

    Establish bill.com workspace
    Also includes npm audit fixes

--- dist/bill_com/633.index.js
+++ dist/bill_com/633.index.js
@@ -1,16 +1,16 @@
-export const id = 357;
-export const ids = [357];
+export const id = 633;
+export const ids = [633];
 export const modules = {
 
-/***/ 9357:
+/***/ 8633:
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "toFormData": () => (/* binding */ toFormData)
 /* harmony export */ });
-/* harmony import */ var fetch_blob_from_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6954);
-/* harmony import */ var formdata_polyfill_esm_min_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6506);
+/* harmony import */ var fetch_blob_from_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(4137);
+/* harmony import */ var formdata_polyfill_esm_min_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5808);
 
 
 
--- src/bill_com/index.js
+++ src/bill_com/index.js
@@ -1,14 +1,14 @@
 /** @fileoverview Entrypoint for choosing which file to run. */
 
-import * as billComIntegrationCreateApprover from './bill_com/bill_com_integration/create_approver.js';
-import * as billComIntegrationCreateBill from './bill_com/bill_com_integration/create_bill.js';
-import * as billComIntegrationSync from './bill_com/bill_com_integration/sync.js';
-import * as billComIntegrationSyncBills from './bill_com/bill_com_integration/sync_bills.js';
-import * as billComIntegrationSyncInternalCustomers from './bill_com/bill_com_integration/sync_internal_customers.js';
-import * as doorKnockingCreateVendor from './bill_com/door_knocking/create_vendor.js';
-import {error} from './common/github_actions_core.js';
-import {fileId} from './bill_com/common/inputs.js';
-import {getApi} from './bill_com/common/api.js';
+import * as billComIntegrationCreateApprover from './bill_com_integration/create_approver.js';
+import * as billComIntegrationCreateBill from './bill_com_integration/create_bill.js';
+import * as billComIntegrationSync from './bill_com_integration/sync.js';
+import * as billComIntegrationSyncBills from './bill_com_integration/sync_bills.js';
+import * as billComIntegrationSyncInternalCustomers from './bill_com_integration/sync_internal_customers.js';
+import * as doorKnockingCreateVendor from './door_knocking/create_vendor.js';
+import {error} from '../common/github_actions_core.js';
+import {fileId} from './common/inputs.js';
+import {getApi} from './common/api.js';
 
 let imp;
 switch (fileId()) {
