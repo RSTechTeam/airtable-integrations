b64b633ba4f8630300754260f108b2b76ef80db1
JustisAllen <justisallen@me.com>
2025-02-12 08:12:01 -0800

    Further buffer bill spend expense rate limit count

--- src/bill_spend_expense/index.js
+++ src/bill_spend_expense/index.js
@@ -15,7 +15,7 @@ import {run} from '../common/action.js';
 
 /** The ~rate limit for BILL Spend & Expense API calls. */
 const throttledFetch =
-    pThrottle({limit: 59, interval: 62 * 1000, strict: true})(fetch);
+    pThrottle({limit: 58, interval: 62 * 1000, strict: true})(fetch);
 
 /**
  * @param {string} endpoint
