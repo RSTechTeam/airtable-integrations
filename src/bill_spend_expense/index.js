d10d2e6a11003a24e79d544e1226dab17e1ad662
JustisAllen <justisallen@me.com>
2025-02-14 11:18:57 -0800

    Further buffer bill spend expense rate limit

--- src/bill_spend_expense/index.js
+++ src/bill_spend_expense/index.js
@@ -14,8 +14,7 @@ import {logJson} from '../common/github_actions_core.js';
 import {run} from '../common/action.js';
 
 /** The ~rate limit for BILL Spend & Expense API calls. */
-const throttledFetch =
-    pThrottle({limit: 58, interval: 65 * 1000, strict: true})(fetch);
+const throttledFetch = pThrottle({limit: 58, interval: 70 * 1000})(fetch);
 
 /**
  * @param {string} endpoint
