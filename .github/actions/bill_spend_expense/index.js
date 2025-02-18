51409fc5077bd29cab7b8f240391d73266ccacc5
JustisAllen <justisallen@me.com>
2025-02-17 18:20:45 -0800

    Try bill spend api carryover concurrency count

--- .github/actions/bill_spend_expense/index.js
+++ .github/actions/bill_spend_expense/index.js
@@ -19721,7 +19721,12 @@ __nccwpck_require__.a(__webpack_module__, async (__webpack_handle_async_dependen
 
 /** The ~rate limit for BILL Spend & Expense API calls. */
 const queue =
-    new p_queue__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .Z({concurrency: 58, intervalCap: 58, interval: 62 * 1000});
+    new p_queue__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .Z({
+      concurrency: 60,
+      intervalCap: 60,
+      interval: 60 * 1000,
+      carryoverConcurrencyCount: true,
+    });
 
 /**
  * @param {string} endpoint
