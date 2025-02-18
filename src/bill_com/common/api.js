a6812a3444a11d0370f67f41d9f7ed4f338d839d
JustisAllen <justisallen@me.com>
2025-02-17 17:30:18 -0800

    Try fix data reference

--- src/bill_com/common/api.js
+++ src/bill_com/common/api.js
@@ -181,7 +181,7 @@ export class Api {
   dataCall(endpoint, data) {
     return this.call(
         endpoint,
-        new URLSearchParams({data: encodeURIComponent(JSON.stringify(data))}));
+        new URLSearchParams({'data': encodeURIComponent(JSON.stringify(data))}));
   }
 
   /**
